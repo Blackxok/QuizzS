4e471de52f73a16fbfbe6deafa280dd7eb9fac95
Blackxok <nasridoni1@gmail.com>
2024-05-10 17:12:11 +0500

    some valid

---
+++ js/input_data.js
@@ -0,0 +1,27 @@
+let Up_name = document.getElementById("Up_name");
+let Up_email = document.getElementById("Up_email");
+
+// name Validation;
+isValidName = (e) => {
+   let er = /^[a-zA-Z0-9_]{3,16}$/;
+   return er.test(e);
+};
+function validName() {
+   if (isValidName(Up_name.value)) {
+      Up_name.classList.remove("wrong");
+   } else {
+      Up_name.classList.add("wrong");
+   }
+}
+// email validation
+isValidEmail = (e) => {
+   let pa = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+   return !!e.match(pa);
+};
+function validEmail() {
+   if (isValidEmail(Up_email.value)) {
+      Up_email.classList.remove("wrong");
+   } else {
+      Up_email.classList.add("wrong");
+   }
+}
